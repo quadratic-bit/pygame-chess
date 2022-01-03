622d699a728fe665de40feaa449b5f8e243d398e
Artem <fynjyxbrjd@gmail.com>
2022-01-03 14:17:51 +0700

    Refactored images and sounds locations, utils.py updated as well

--- chess/utils.py
+++ chess/utils.py
@@ -5,7 +5,7 @@ import pygame
 
 def load_image(name: str) -> pygame.surface.Surface:
     """Load image as surface"""
-    path = f"chess/data/{name}"
+    path = f"chess/data/images/{name}"
     if not os.path.isfile(path):
         raise ValueError(f"File {path} is not found")
     return pygame.image.load(path).convert_alpha()
