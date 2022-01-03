006df00720b5c242e153583a31224dfa59aedfc5
Artem <fynjyxbrjd@gmail.com>
2022-01-03 14:16:43 +0700

    Added load functions to utils.py

--- chess/utils.py
+++ chess/utils.py
@@ -9,3 +9,11 @@ def load_image(name: str) -> pygame.surface.Surface:
     if not os.path.isfile(path):
         raise ValueError(f"File {path} is not found")
     return pygame.image.load(path).convert_alpha()
+
+
+def load_sound(name: str) -> pygame.mixer.Sound:
+    return pygame.mixer.Sound(f"chess/data/sounds/{name}")
+
+
+def load_font(name: str, size: int) -> pygame.font.Font:
+    return pygame.font.Font(f"chess/data/fonts/{name}", size)
